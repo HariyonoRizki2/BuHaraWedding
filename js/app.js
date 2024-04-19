b1de38f474324e2b0f9ac7b90786e59e7d9abedf
HARIYONO RIZKI <smkn1dlanggu@yahoo.com>
2024-04-19 17:25:24 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -31,44 +31,44 @@ const storage = (table) => {
     };
 };
 
-const request = (method, path) => {
+//const request = (method, path) => {
 
-    let url = document.querySelector('body').getAttribute('data-url');
-    let req = {
-        method: method,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    };
+   // let url = document.querySelector('body').getAttribute('data-url');
+    //let req = {
+      //  method: method,
+      //  headers: {
+        //    'Accept': 'application/json',
+       //     'Content-Type': 'application/json'
+       // }
+   // };
 
-    if (url.slice(-1) == '/') {
-        url = url.slice(0, -1);
-    }
+   // if (url.slice(-1) == '/') {
+  //      url = url.slice(0, -1);
+  //  }
 
-    return {
-        async then(...params) {
-            return fetch(url + path, req)
-                .then((res) => res.json())
-                .then((res) => {
-                    if (res.error !== null) {
-                        throw res.error[0];
-                    }
+  //  return {
+    //    async then(...params) {
+         //   return fetch(url + path, req)
+           //     .then((res) => res.json())
+             //   .then((res) => {
+               //     if (res.error !== null) {
+                //        throw res.error[0];
+                //     }
 
-                    return res;
-                })
-                .then(...params);
-        },
-        token(token) {
-            req.headers['Authorization'] = 'Bearer ' + token;
-            return this;
-        },
-        body(body) {
-            req.body = JSON.stringify(body);
-            return this;
-        },
-    };
-};
+                 //   return res;
+               // })
+              //  .then(...params);
+       // },
+      //  token(token) {
+         //   req.headers['Authorization'] = 'Bearer ' + token;
+        //    return this;
+     //  },
+      //  body(body) {
+        //    req.body = JSON.stringify(body);
+       //     return this;
+     //   },
+   // };
+//};
 
 const util = (() => {
 
